dfc31a35dade50efe42f47355ce8c25077a06696
bindu8601 <127506733+bindu8601@users.noreply.github.com>
2024-11-18 14:07:32 +0530

    connecting to database

--- server/app.js
+++ server/app.js
@@ -1,7 +1,17 @@
 const express=require('express')
+const dotenv=require('dotenv')
+const db = require('./src/config/db');
 const app=express()
+dotenv.config()
 app.use('/',(req,res,next)=>{
     res.send('<h1>Hello</h1')
 })
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        process.exit(1);
+    }
+    console.log('Connected to MySQL!');
+});
 
 app.listen(3001)
