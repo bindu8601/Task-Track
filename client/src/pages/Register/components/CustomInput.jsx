778ca0f0f906f63a69cccbfc2be17f95f6f7938b
bindu8601 <127506733+bindu8601@users.noreply.github.com>
2024-11-18 11:13:45 +0530

    register page

---
+++ client/src/pages/Register/components/CustomInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Input } from "antd";
+
+
+const CustomInput = ({placeHolder}) => {
+  return (
+    <div className='pt-4'>
+      <Input className='w-[559px] h-[40px]' placeholder={placeHolder}/>
+    </div>
+  )
+}
+
+export default CustomInput
